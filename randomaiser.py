8cc3791dc5b3fefef0460eb1ec9ea82f68fe6ff1
ArTmIx9 <artembilancuk@gmail.com>
2024-02-08 20:20:28 +0200

    ver(0.0.3)Random

--- randomaiser.py
+++ randomaiser.py
@@ -1,5 +1,7 @@
 from PyQt5.QtCore import Qt 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from random import randint
+
 
 app = QApplication([])
 window = QWidget()
@@ -17,6 +19,13 @@ vline.addWidget(text1, alignment=Qt.AlignCenter)
 vline.addWidget(text2, alignment=Qt.AlignCenter)
 vline.addWidget(button, alignment=Qt.AlignCenter)
 window.setLayout(vline)
+
+#Функціонал
+def random_number():
+    text1.setText("Переможець")
+    x = str(randint(1, 100))
+    text2.setText(x)
+button.clicked.connect(random_number)
 
 window.show()
 app.exec_()
