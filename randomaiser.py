10c1b447a4d45a448ae69a70e3887d40da4fa4bd
ArTmIx9 <artembilancuk@gmail.com>
2024-02-08 19:56:09 +0200

    ver 0.0.2(new 3 widgets)

--- randomaiser.py
+++ randomaiser.py
@@ -1,9 +1,22 @@
 from PyQt5.QtCore import Qt 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 app = QApplication([])
 window = QWidget()
+window.setWindowTitle("Randomaiser")
+window.resize(700,600)
 
+#Створення віджетів
+text1 = QLabel("Натисни щобдізнатися переможця")
+text2 = QLabel("?")
+button = QPushButton("Нитисни щоб згенерувати число")
+
+#Роташування віджетів
+vline = QVBoxLayout()
+vline.addWidget(text1, alignment=Qt.AlignCenter)
+vline.addWidget(text2, alignment=Qt.AlignCenter)
+vline.addWidget(button, alignment=Qt.AlignCenter)
+window.setLayout(vline)
 
 window.show()
 app.exec_()
